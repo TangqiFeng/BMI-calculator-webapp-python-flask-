82dbb58e61cbb0334e610c08188b2e0197badbf1
TangqiFeng <kylezete@gmail.com>
2016-11-27 18:44:46 +0000

    update

--- webapp.py
+++ webapp.py
@@ -8,20 +8,37 @@ app.secret_key='123'
 
 @app.route('/')
 def hello_world():
+    indexCss=url_for("static",filename="css/indexCss.css")
     mainJs=url_for("static",filename="js/main_part.js")
     loginButton=url_for("static",filename="images/loginButton.png")
     login=url_for("static",filename="images/login.jpg")
-    return render_template("index.html",mainJs=mainJs,loginButton=loginButton,login=login)
+    titlePic=url_for("static",filename="images/title.png")
+    ex1=url_for("static",filename="images/ex1.jpg")
+    ex2=url_for("static",filename="images/ex2.jpg")
+    ex3=url_for("static",filename="images/ex3.png")
+    titleBg=url_for("static",filename="images/titleBg.png")
+    modelBg=url_for("static",filename="images/modelBg.png")
+    bg=url_for("static",filename="images/bg.jpg")
+    return render_template("index.html",mainJs=mainJs,indexCss=indexCss,loginButton=loginButton,login=login,titlePic=titlePic,ex1=ex1,ex2=ex2,ex3=ex3,titleBg=titleBg,modelBg=modelBg,bg=bg)
 
 
 
 @app.route('/login', methods=['POST'])
 
 def login():
+    indexCss=url_for("static",filename="css/indexCss.css")
+    loginedCss=url_for("static",filename="css/loginedCss.css")
     mainJs=url_for("static",filename="js/main_part.js")
     convertJs=url_for("static",filename="js/convert.js")
     loginButton=url_for("static",filename="images/loginButton.png")
     login=url_for("static",filename="images/login.jpg")
+    titlePic=url_for("static",filename="images/title.png")
+    ex1=url_for("static",filename="images/ex1.jpg")
+    ex2=url_for("static",filename="images/ex2.jpg")
+    ex3=url_for("static",filename="images/ex3.png")
+    titleBg=url_for("static",filename="images/titleBg.png")
+    modelBg=url_for("static",filename="images/modelBg.png")
+    bg=url_for("static",filename="images/bg.jpg")
     form = request.form
     username = form.get('username')
     password = form.get('password')
@@ -29,10 +46,10 @@ def login():
     
     if not username:
         flash("please input username !")
-        return render_template("index.html",convertJs=convertJs,mainJs=mainJs,loginButton=loginButton,login=login)
+        return render_template("index.html",convertJs=convertJs,indexCss=indexCss,mainJs=mainJs,loginButton=loginButton,login=login,titlePic=titlePic,ex1=ex1,ex2=ex2,ex3=ex3,titleBg=titleBg,modelBg=modelBg,bg=bg)
     elif not password:
         flash("please input password !")
-        return render_template("index.html",convertJs=convertJs,mainJs=mainJs,loginButton=loginButton,login=login)
+        return render_template("index.html",convertJs=convertJs,indexCss=indexCss,mainJs=mainJs,loginButton=loginButton,login=login,titlePic=titlePic,ex1=ex1,ex2=ex2,ex3=ex3,titleBg=titleBg,modelBg=modelBg,bg=bg)
     else:
         sql = 'SELECT * FROM mytable'
         conn = sqlite3.connect('data/data.db')
@@ -46,23 +63,27 @@ def login():
             pw = row[2]
             if (username == un and password == pw):
                 flash(row[1])
-                return render_template("logined.html",convertJs=convertJs,mainJs=mainJs,loginButton=loginButton,login=login)
+                return render_template("logined.html",convertJs=convertJs,loginedCss=loginedCss,mainJs=mainJs,loginButton=loginButton,login=login,ex3=ex3,titlePic=titlePic,titleBg=titleBg,modelBg=modelBg,bg=bg)
                 cur.close()
                 conn.close()
         
         flash("user name or password is wrong !")
-        return render_template("index.html",convertJs=convertJs,mainJs=mainJs,loginButton=loginButton,login=login)
+        return render_template("index.html",convertJs=convertJs,indexCss=indexCss,mainJs=mainJs,loginButton=loginButton,login=login,titlePic=titlePic,ex1=ex1,ex2=ex2,ex3=ex3,titleBg=titleBg,modelBg=modelBg,bg=bg)
         cur.close()
         conn.close()
             
 @app.route('/register')
 def registerNow():
+    registerCss=url_for("static",filename="css/registerCss.css")
+    registerBkg=url_for("static",filename="images/registerBg.jpg")
     registerButton=url_for("static",filename="images/registerButton.png")
     register=url_for("static",filename="images/register.png")
-    return render_template("register.html",registerButton=registerButton,register=register)
+    return render_template("register.html",registerCss=registerCss,registerButton=registerButton,register=register,registerBkg=registerBkg)
     
 @app.route('/register', methods=['POST'])
 def register():
+    registerCss=url_for("static",filename="css/registerCss.css")
+    registerBkg=url_for("static",filename="images/registerBg.jpg")
     registerButton=url_for("static",filename="images/registerButton.png")
     register=url_for("static",filename="images/register.png")
     form = request.form
@@ -71,10 +92,10 @@ def register():
     
     if not username:
         flash("please input username !")
-        return render_template("register.html",registerButton=registerButton,register=register)
+        return render_template("register.html",registerCss=registerCss,registerButton=registerButton,register=register,registerBkg=registerBkg)
     elif not password:
         flash("please input password !")
-        return render_template("register.html",registerButton=registerButton,register=register)
+        return render_template("register.html",registerCss=registerCss,registerButton=registerButton,register=register,registerBkg=registerBkg)
     else:
         sql = str('INSERT INTO mytable(name,password) VALUES (?,?)')
         conn = sqlite3.connect('data/data.db')
@@ -84,11 +105,13 @@ def register():
         cur.close()
         conn.close()
         flash("success !")
-        return render_template("register.html",registerButton=registerButton,register=register)
+        return render_template("register.html",registerCss=registerCss,registerButton=registerButton,register=register,registerBkg=registerBkg)
     
 @app.route('/moreInfo')
 def init():
-    return render_template("moreInfo.html");
+    registerBkg=url_for("static",filename="images/registerBg.jpg")
+    moreInfoCss=url_for("static",filename="css/moreInfoCss.css")
+    return render_template("moreInfo.html",moreInfoCss=moreInfoCss,registerBkg=registerBkg);
 
 if __name__ == '__main__':
     app.run()
